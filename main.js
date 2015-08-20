87d769fbc5e64e529c1dd3e454ffa9a773c1e0e2
tvance929 <toddvance@gmail.com>
2015-08-20 13:38:09 -0500

    testing github desktop
    testing - just added a simple word to the js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var TODOS = ["buy food", "make food", "eat food"];
 
 var TodoApp = React.createClass({
     render: function(){
-        return(<div>{this.props.todos}</div>);
+        return(<div>{this.props.todos}hi</div>);
     },
 });
 
